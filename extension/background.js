7f047dc83c393bd63c1c356d1a4e03b102c25fc4
oksana.misiura.cld@gmail.com <oksana.misiura.cld@gmail.com>
2025-02-23 14:50:57 +0000

    Update browser extension to send a post request

--- extension/background.js
+++ extension/background.js
@@ -1,14 +1,10 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === "sendHTML") {
-        console.log("Получен HTML:", message.data);
-
-        fetch("http://localhost:8080/collect", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ html: message.data })
-        })
-        .then(response => response.json())
-        .then(data => console.log("Ответ сервера:", data))
-        .catch(error => console.error("Ошибка:", error));
-    }
-});
+chrome.browserAction.onClicked.addListener(function (tab) {
+    chrome.windows.create({
+      url: chrome.runtime.getURL('popup.html'),
+      type: 'popup',
+      height: 600,
+      width: 400,
+      focused: true
+    });
+  });
+  
